0a70eeb631cb369a430a86b07e6bff8c5c32a5ae
4evsec <179505699+4evsec@users.noreply.github.com>
2025-11-12 00:12:45 +0100

    refactor(payload.template.js): update payload

--- bin/payload.template.js
+++ bin/payload.template.js
@@ -1,5 +1,4 @@
-fetch('{{URL}}', JSON.parse('{{OPTIONS_JSON}}')).then(async (response) => {
-  const { status, statusText, headers } = response;
-  const body = await response.bytes().then(Object.values);
+fetch('{{URL}}', JSON.parse('{{OPTIONS_JSON}}')).then(async ({ status, statusText, headers, ...r }) => {
+  const body = await r.bytes().then(Object.values);
   return { status, statusText, headers: Object.fromEntries([...headers.entries()]), body };
 });
